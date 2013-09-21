c6ee294bcfb3d6a0a1c2ec2e45fb8dda37b320f2
Petri Kainulainen <petri.kainulainen@gmail.com>
2013-09-21 21:55:50 +0300

    Removed unused import

--- sign-in/spring-mvc-normal/src/main/java/net/petrikainulainen/spring/social/signinmvc/config/SecurityContext.java
+++ sign-in/spring-mvc-normal/src/main/java/net/petrikainulainen/spring/social/signinmvc/config/SecurityContext.java
@@ -6,7 +6,6 @@ import net.petrikainulainen.spring.social.signinmvc.user.repository.UserReposito
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
